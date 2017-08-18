d6477d4780d3952c777b7536b48779190af6f2d9
Alex Jung <alexandermjung89@gmail.com>
2017-08-18 17:54:02 -0500

    forgot to create branch

--- server/apps.js
+++ server/apps.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 //need path to module
 
 var port = 5000;
 
 app.use(express.static('public'));
-//need body parser for angular yet
+app.use(bodyParser.json());
 
 //app.use(/*add the request catcher in quotes'', then the modules path*/);
 
---
+++ server/modules/pool.js
@@ -0,0 +1,11 @@
+var pg = require('pg');
+
+var config = {
+    database: 'weekend-challenge-4',
+    host ='localhost',
+    port= 5432,
+    max= 10,
+    idleTimeoutMillis = 4000
+};
+
+module.exports=pg.Pool(config);
