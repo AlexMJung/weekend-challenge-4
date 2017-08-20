f849a639200e5de71d531128a189defd870f6aa9
Alex Jung <alexandermjung89@gmail.com>
2017-08-20 09:51:57 -0500

    data added to db and loading to dom

--- public/client.js
+++ public/client.js
@@ -11,19 +11,26 @@ app.controller('EmployeeController', ['$http', function ($http) {
 
 
     self.getEmployees = function () {
-
-
         $http({
             method: 'GET',
             url: '/employees'
         }).then(function (response) {
             self.employees = response.data;
-            console.log (response.data);
-            console.log (self.employees);
         });//end of then
     };//end of self.getEmployees
 
-
+    self.postNewEmployee = function () {
+        console.log('post hit');
+        
+        $http({
+            method: 'POST',
+            url: '/employees',
+            data: self.newEmployee
+        }).then(function(response){
+            console.log(response);
+            self.getEmployees();
+        });//end of then
+    };//end of self.postNewEmployee
     
 
     self.getEmployees();
