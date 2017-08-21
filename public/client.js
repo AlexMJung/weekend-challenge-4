28469a172eef0defacd6ca57b1c6b43034574ac1
Alex Jung <alexandermjung89@gmail.com>
2017-08-20 21:58:04 -0500

    base mode working

--- public/client.js
+++ public/client.js
@@ -8,30 +8,40 @@ app.controller('EmployeeController', ['$http', function ($http) {
     var self = this;
 
     self.employees = [];
+    self.newEmployee = {};
 
 
     self.getEmployees = function () {
+        self.annualCost = 0;
         $http({
             method: 'GET',
             url: '/employees'
         }).then(function (response) {
             self.employees = response.data;
+            for (var i = 0; i < response.data.length; i++) {
+                var element = response.data[i];
+            //     self.annualCost.push(element.salary)                
+            self.annualCost = self.annualCost + element.salary
+        }
+            console.log(self.annualCost);
+            self.monthlyCost = self.annualCost/12
         });//end of then
     };//end of self.getEmployees
 
     self.postNewEmployee = function () {
         console.log('post hit');
-        
-        $http({
+                $http({
             method: 'POST',
             url: '/employees',
             data: self.newEmployee
         }).then(function(response){
             console.log(response);
             self.getEmployees();
-        });//end of then
+            self.newEmployee = {};
+            });//end of then
     };//end of self.postNewEmployee
     
+
 
     self.getEmployees();
 
